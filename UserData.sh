cdef0c0ec91176d610cc07f2e2b8010ada5b40fc
raushan8586 <72825046+raushan8586@users.noreply.github.com>
2025-04-26 20:38:26 +0530

    Update UserData.sh

--- UserData.sh
+++ UserData.sh
@@ -4,3 +4,13 @@ yum install -y httpd
 systemctl start httpd
 systemctl enable httpd
 echo "<h1>Hello World from $(hostname -f)</h1>" > /var/www/html/index.html
+
+#!/bin/bash
+apt update -y
+apt install -y apache2
+systemctl start apache2
+systemctl enable apache2
+echo "<h1>Hello World from $(hostname -f)</h1>" > /var/www/html/index.html
+
+
+
